1dc9c31e70011772c60a1497a8257922afd3897f
Arnold Galovics <galovicsarnold@gmail.com>
2018-02-14 22:02:33 +0100

    Minor fine tuning

--- translator/src/main/java/com/arnoldgalovics/blog/feignheader/translator/controller/TranslationController.java
+++ translator/src/main/java/com/arnoldgalovics/blog/feignheader/translator/controller/TranslationController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController("/message")
 public class TranslationController {
     @RequestMapping
     public MessageResponse message(@RequestHeader(value = "Accept-Language") String language) {
         String message = "english";
-        if ("de".equalsIgnoreCase(language)) {
+        if (Locale.GERMANY.equals(Locale.forLanguageTag(language))) {
             message = "german";
         }
         return new MessageResponse(message);
